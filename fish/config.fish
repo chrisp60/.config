94432c66b9749b5bbbf2c3986906c0450f89b4f3
Christopher Perry <christopher.perry410@gmail.com>
2023-04-28 19:42:29 -0400

    Feat(fish): alias, vi_mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 fish_config theme choose "Catppuccin Mocha"
+alias lg='lazygit'
+alias lsa='ls -al'
+fish_vi_key_bindings insert
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
