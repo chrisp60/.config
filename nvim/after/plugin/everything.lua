048b76f452b7ade4af9f859805d2a21868e234aa
Chris Pery <email@cperry.me>
2023-08-01 17:58:48 -0400

    feat(nvim): keybinding, refinement

--- nvim/after/plugin/everything.lua
+++ nvim/after/plugin/everything.lua
@@ -2,8 +2,15 @@
 ---@diagnostic disable: undefined-global
 
 local lsp = require("lsp-zero")
-lsp.preset({
-    name = "minimal",
+lsp.preset({ name = "minimal" })
+lsp.format_on_save({
+    format_opts = { async = true, timeout_ms = 10000, },
+    servers = {
+        ['lua_ls'] = { 'lua' },
+        ['rust_analyzer'] = { 'rust' },
+        ['null-ls'] = { 'markdown' },
+        ['taplo'] = { 'toml' },
+    }
 })
 
 local cmp = require('cmp')
@@ -17,7 +24,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
         }
     } }),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ['<C-l>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
 })
@@ -32,82 +39,44 @@ lsp.setup_nvim_cmp({
     completion = { completeopt = 'menu,menuone,noselectpreview' },
     mapping = cmp_mappings,
     sources = {
-        { name = 'nvim_lsp', group_index = 1 },
-        { name = 'luasnip',  group_index = 2 },
-        { name = 'buffer',   keyword_length = 3, group_index = 4 },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip', },
+        { name = 'buffer' },
         { name = 'path' },
     },
     experimental = { ghost_text = true },
 })
 
-lsp.setup() -- Must be called before native lsp
-
-local min_error = { severity = { min = vim.diagnostic.severity.WARN } }
+lsp.setup()
 
 lsp.on_attach(function(client, bufnr)
     -- client.server_capabilities.semanticTokensProvider = nil
-    vim.keymap.set({ 'n', 'v' }, 'K', function() vim.lsp.buf.hover() end, opts)
-
-    vim.keymap.set('n', 'gdd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'gdj', function() vim.diagnostic.goto_next(min_error) end, opts)
-    vim.keymap.set('n', 'gdk', function() vim.diagnostic.goto_prev(min_error) end, opts)
-
-    -- Set qf list but dont open it, just go to the first
-    local function on_list(options)
-        vim.fn.setqflist({}, ' ', options)
-        vim.api.nvim_command('cfirst')
-    end
-
-    vim.keymap.set('n', 'gvr', function() vim.lsp.buf.references(nil, { on_list = on_list }) end, opts)
-    vim.keymap.set('n', 'gvS', function() vim.lsp.buf.document_symbol({ on_list = on_list }) end, opts)
-    vim.keymap.set('n', 'gvs', function() vim.lsp.buf.workspace_symbol(nil, { on_list = on_list }) end, opts)
-
-    vim.keymap.set('n', 'gar', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set({ 'v', 'n' }, 'gaa', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set({ 'v', 'n' }, 'gaf', function() vim.lsp.buf.format { async = false } end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gn', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', 'gp', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>r', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set({ 'n', 'v', 'x' }, 'ga', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>h', function() vim.lsp.buf.format({ async = true }) end, opts)
 end)
 
-vim.keymap.set({ 'n', 'v' }, 'gsj', '<cmd>cnext<cr>', {}, opts)
-vim.keymap.set({ 'n', 'v' }, 'gsk', '<cmd>cprev<cr>', {}, opts)
-vim.keymap.set({ 'n', 'v' }, 'gsx', '<cmd>cclose<cr>', {}, opts)
-
 vim.diagnostic.config({
-    virtual_text = min_error,
     update_in_insert = true,
+    virtual_text = true,
     underline = false,
     signs = false,
     float = false,
 })
 
 local builtin = require('telescope.builtin')
+require('telescope').setup({})
 
-require('telescope').setup({
-    defaults = {
-        border = true,
-        layout_strategy = 'bottom_pane',
-        layout_config = {
-            bottom_pane = {
-                prompt_position = 'bottom',
-                height = 0.99,
-                width = 0.99,
-            },
-        }
-    },
-})
-
-vim.keymap.set('n', '<leader>F', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>H', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>q', builtin.quickfix, {})
-vim.keymap.set('n', '<leader>h', builtin.command_history, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>l', builtin.loclist, {})
+vim.keymap.set('n', '<leader>R', builtin.registers, {})
 vim.keymap.set('n', '<leader>s', builtin.lsp_workspace_symbols, {})
-vim.keymap.set('n', '<leader>j', function()
-    builtin.jumplist({ fname_width = 100 })
-end, {})
 
 -- Plain lines and minimal flair, please.
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
@@ -125,9 +94,8 @@ require 'nvim-treesitter.configs'.setup {
 require("harpoon").setup({ menu = { width = vim.api.nvim_win_get_width(0) - 4, } })
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
-vim.keymap.set('n', 'g.', mark.add_file)
-vim.keymap.set('n', 'gm', ui.toggle_quick_menu)
-vim.keymap.set('n', 'gn', function() ui.nav_next() end)
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<leader>m', ui.toggle_quick_menu)
 vim.keymap.set('n', 'gj', function() ui.nav_file(1) end)
 vim.keymap.set('n', 'gk', function() ui.nav_file(2) end)
 vim.keymap.set('n', 'gl', function() ui.nav_file(3) end)
