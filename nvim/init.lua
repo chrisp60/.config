be63755251936b1b8e1299a01d8c641508c36b4e
Chris Pery <email@cperry.me>
2023-09-05 16:58:13 -0400

    rm(nvim): neoorg

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-require('set')
-
 ---@diagnostic disable: undefined-global
+---
+require('set')
 
 -- Bootstrap Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -49,26 +49,6 @@ local plugins = {
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'L3MON4D3/LuaSnip' },
         }
-    },
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("neorg").setup {
-                load = {
-                    ["core.defaults"] = {},  -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = {      -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                            },
-                        },
-                    },
-                },
-            }
-        end,
     },
     'NvChad/nvim-colorizer.lua',
     'theprimeagen/harpoon',
