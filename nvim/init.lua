16fcecb0274aa2509fe2c25e09cb0ef9a23b6977
chrisp60 <gh@cperry.me>
2023-11-20 00:35:17 -0500

    maps

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,7 @@ vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
 vim.opt.smartindent = true
 vim.opt.softtabstop = 4
+vim.opt.splitright = true
 vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
@@ -97,14 +98,16 @@ local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
+
     local function opts_desc(desc)
         return { buffer = bufnr, remap = false, desc = desc }
     end
 
-    vim.cmd([[autocmd BufWritePre *.lua lua require('stylua').format()]])
-
     lsp_zero.buffer_autoformat()
 
+    vim.keymap.set("n", "<leader>o", function()
+        vim.lsp.buf.format({ async = false })
+    end, opts_desc("Go to Definition [LSP]"))
     vim.keymap.set("n", "gd", function()
         vim.lsp.buf.definition()
     end, opts_desc("Go to Definition [LSP]"))
@@ -129,7 +132,7 @@ lsp_zero.on_attach(function(client, bufnr)
         vim.diagnostic.goto_prev()
     end, opts_desc("Previous Diagnostic [LSP]"))
 
-    vim.keymap.set("n", "<leader>a", function()
+    vim.keymap.set({ "n", "v", "x" }, "ga", function()
         vim.lsp.buf.code_action()
     end, opts_desc("Code Action [LSP]"))
 
@@ -144,7 +147,11 @@ lsp_zero.on_attach(function(client, bufnr)
     vim.keymap.set("i", "<C-h>", function()
         vim.lsp.buf.signature_help()
     end, opts_desc("Signature Help [LSP]"))
+
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.setqflist, opts_desc("Set qflist Diagnostics [LSP]"))
 end)
+
+vim.diagnostic.config({ virtual_text = true, underline = false })
 
 -- Mason & LSP
 require("mason").setup({})
@@ -195,7 +202,7 @@ cmp.setup({
         ["<C-p>"] = cmp_action.luasnip_jump_backward(),
 
         -- Invoke cmp menu manually
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-t>"] = cmp.mapping.complete(),
     }),
     formatting = require("lsp-zero").cmp_format(),
 })
@@ -209,7 +216,7 @@ vim.keymap.set(
 )
 
 -- Git Fugitive
-vim.keymap.set("n", "<leader>g", "<cmd>vert Git<cr>", { desc = "Git Fugitive in vertical window" })
+vim.keymap.set("n", "<leader>G", "<cmd>vert Git<cr>", { desc = "Git Fugitive in vertical window" })
 
 -- Telescope
 local builtin = require("telescope.builtin")
@@ -221,15 +228,15 @@ require("telescope").setup({
     },
 })
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files [telescope]" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep live [telescope]" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers [telescope]" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help tags [telescope]" })
-vim.keymap.set("n", "<leader>fH", function()
+vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find Files [telescope]" })
+vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Grep live [telescope]" })
+vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Find buffers [telescope]" })
+vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Find Help tags [telescope]" })
+vim.keymap.set("n", "<leader>H", function()
     builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
 end, { desc = "Find Hidden files [telescope]" })
-vim.keymap.set("n", "<leader>f?", builtin.keymaps, { desc = "Find Keymaps [telescope]" })
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find diagnostics [telescope]" })
+vim.keymap.set("n", "<leader>?", builtin.keymaps, { desc = "Find Keymaps [telescope]" })
+vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "Find diagnostics [telescope]" })
 
 vim.keymap.set("n", "<leader>O", function()
     vim.cmd([[silent %! prettierd %]])
@@ -239,7 +246,7 @@ end, { desc = "Format with prettierd [prettierd]" })
 local harpoon_mark = require("harpoon.mark")
 local harpoon_ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<C-a>", function()
+vim.keymap.set("n", "gA", function()
     harpoon_mark.add_file()
 end, { desc = "Add File [Harpoon]" })
 
@@ -247,9 +254,9 @@ vim.keymap.set("n", "<C-p>", function()
     harpoon_ui.nav_prev()
 end, { desc = "Next File [Harpoon]" })
 
-vim.keymap.set("n", "<C-m>", function()
+vim.keymap.set("n", "gm", function()
     harpoon_ui.toggle_quick_menu()
-end, { desc = "Toggle Quick Menu [Harpoon]" })
+end, { desc = "Toggle Quick Menu [Harpoon]", remap = false, silent = false })
 
 vim.keymap.set("n", "<C-n>", function()
     harpoon_ui.nav_next()
@@ -259,5 +266,5 @@ end)
 for int = 0, 9 do
     vim.keymap.set("n", tostring(int), function()
         harpoon_ui.nav_file(int)
-    end)
+    end, { desc = "Go to " .. tostring(int) .. " Harpoon file [Harpoon]" })
 end
