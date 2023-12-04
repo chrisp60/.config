b2a9bcb3181527d1babdd21a86f4370706579a40
chrisp60 <gh@cperry.me>
2023-12-04 15:45:45 -0500

    nvim: fix regex highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,6 +68,7 @@ require("lazy").setup({
                 auto_install = true,
                 sync_install = false,
                 highlight = { enable = true },
+                additional_vim_regex_highlighting = false,
             })
         end,
     },
