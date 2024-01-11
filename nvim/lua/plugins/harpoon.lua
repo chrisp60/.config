4264d37541bd83b69bffde9857c9222cdc24bb3c
chrisp60 <gh@cperry.me>
2024-01-11 07:56:06 -0500

    nvim: change harpoon mapping

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,12 +4,16 @@ return {
         config = function()
             local mark = require("harpoon.mark")
             local ui = require("harpoon.ui")
-            vim.keymap.set("n", "gA", function() mark.add_file() end)
-            vim.keymap.set("n", "gm", function() ui.toggle_quick_menu() end)
-            vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-            vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-            vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-            vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+            vim.keymap.set("n", "<C-s>a", function() mark.add_file() end)
+            vim.keymap.set("n", "<C-s>m", function() ui.toggle_quick_menu() end)
+
+            vim.keymap.set("n", "<C-s>j", function() ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-s>k", function() ui.nav_file(2) end)
+            vim.keymap.set("n", "<C-s>l", function() ui.nav_file(3) end)
+            vim.keymap.set("n", "<C-s>;", function() ui.nav_file(4) end)
+
+            vim.keymap.set("n", "<C-s>n", function() ui.nav_next() end)
+            vim.keymap.set("n", "<C-s>p", function() ui.nav_prev() end)
         end,
     },
 }
