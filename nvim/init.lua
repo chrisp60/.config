87859b385e7b6ac5184d02ed6ce53c56a18bfa20
chrisp60 <89366859+chrisp60@users.noreply.github.com>
2024-05-04 07:54:44 -0400

    restart

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,77 +1,47 @@
-vim.g.mapleader = " "
+Set = vim.keymap.set 
+O = vim.opt
 
--- Bootstrap Lazy
+vim.g.mapleader = ' '
+
+O.number = true
+O.relativenumber = true
+O.showcmd = true
+O.showmode = false
+O.wrap = false
+O.signcolumn = "yes"
+O.textwidth = 100
+O.cursorline = true
+O.colorcolumn = "100"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
-
-local set = vim.keymap.set
-
-set("n", "<leader>L", "<cmd>luafile %<cr>")
-set("n", "<C-c>n", "<cmd>cn<cr>")
-set("n", "<C-c>p", "<cmd>cp<cr>")
-set("n", "<C-c>o", "<cmd>cope<cr>")
-
-vim.lsp.set_log_level("ERROR")
-vim.opt.undofile = true
-vim.opt.colorcolumn = "80,100"
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.showmode = true
-vim.opt.signcolumn = "yes"
-vim.opt.smartindent = true
-vim.opt.softtabstop = 4
-vim.opt.autoindent = true
-vim.opt.splitright = true
-vim.opt.swapfile = false
-vim.opt.tabstop = 4
-vim.opt.termguicolors = true
-vim.opt.updatetime = 10
-vim.opt.wrap = false
-
-vim.diagnostic.config({
-    virtual_text = true,
-    update_in_insert = true,
-    signs = false,
-    underline = false,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
-    pattern = "*.html",
-    callback = function()
-        vim.cmd([[set ft=htmldjango]])
-        set("n", "<leader>e", function()
-            vim.cmd([[silent !djlint % --quiet --reformat]])
-            vim.cmd([[edit]])
-            vim.cmd([[set ft=htmldjango]])
-        end)
-    end,
-})
-
----@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- Do not show hot-reload messages from Lazy
-require("lazy").setup("plugins", {
-    dev = {
-        path = "~/projects",
-        patterns = { "chrisp60" },
-        fallback = false,
-    },
-    change_detection = {
-        notify = false,
-    },
+require("lazy").setup({
+	-- init.lua:
+	{
+		'nvim-telescope/telescope.nvim', 
+		branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function() 
+			require('telescope').setup()
+			local tele = require('telescope.builtin')
+			Set("n", "<leader>f", tele.find_files)
+			Set("n", "<leader>h", tele.help)
+		end,
+	},
+
+	{ "folke/neodev.nvim" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "tpope/vim-fugitive", keys = { {"n", "<leader>t", "<cmd>Git<cr>" } } },
+
 })
