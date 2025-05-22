fb4c3a3cc2c844de814be6546b5679191fa1a475
chrisp60 <89366859+chrisp60@users.noreply.github.com>
2025-05-22 06:48:10 -0400

    feat(nvim): snippys

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,10 +1,11 @@
-return {
-	"L3MON4D3/LuaSnip",
-	config = function()
-		local ls = require("luasnip")
-		local fmt = require("luasnip.extras.fmt").fmt
-		local bon = fmt(
-			[[
+local snipet = function()
+	local ls = require("luasnip")
+	local t = ls.t
+	local fmt = require("luasnip.extras.fmt").fmt
+	--
+
+	local bon = fmt(
+		[[
         #[bon::bon]
         impl {type} {{
             #[builder(finish_fn = construct)]
@@ -18,32 +19,40 @@ return {
                 )
             }}
         }}
-      ]],
-			{ type = ls.i(1) },
-			{ trim_empty = true, dedent = true }
-		)
+  ]],
+		{ type = ls.i(1) },
+		{ trim_empty = true, dedent = true }
+	)
 
-		local bon_create = fmt(
-			[[
-        #[bon::bon]
-        impl {type} {{
-            #[builder(finish_fn = construct, start_fn = new)]
-            pub async fn create(
-                #[builder(finish_fn)]
-                connection: &mut PgConnection,
-            ) -> sqlx::Result<Self> {{
-                sqlx::query_as_unchecked!(Self,
-                    r#"
-                    "#
-                )
-                .fetch_one(connection)
-                .await
-            }}
-        }}
-      ]],
-			{ type = ls.i(1) },
-			{ trim_empty = true, dedent = true }
-		)
-		ls.add_snippets("rust", { ls.snippet("impl search", bon), ls.snippet("impl create", bon_create) })
-	end,
+	local bon_create = fmt(
+		[[
+      #[bon::bon]
+      impl {type} {{
+          #[builder(finish_fn = construct, start_fn = new)]
+          pub async fn create(
+              #[builder(finish_fn)]
+              connection: &mut PgConnection,
+          ) -> sqlx::Result<Self> {{
+              sqlx::query_as_unchecked!(Self,
+                  r#"
+                  "#
+              )
+              .fetch_one(connection)
+              .await
+          }}
+      }}
+  ]],
+		{ type = ls.i(1) },
+		{ trim_empty = true, dedent = true }
+	)
+	ls.add_snippets("rust", {
+		ls.snippet("impl search", bon),
+		ls.snippet("impl create", bon_create),
+		ls.snippet("#expect", t('#[expect(dead_code, reason = "todo")]')),
+	})
+end
+
+return {
+	"L3MON4D3/LuaSnip",
+	config = snipet,
 }
